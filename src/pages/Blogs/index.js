5beffe461a856e5993856501dac97fd4b7e5e25b
Nijat Dursunlu <nicat.dursunlu@gmail.com>
2022-10-06 15:53:09 +0400

    feature/blogs: Implement searching of blogs

--- src/pages/Blogs/index.js
+++ src/pages/Blogs/index.js
@@ -1,29 +1,42 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button, Input, List } from 'antd'
 import { PlusOutlined, SearchOutlined } from '@ant-design/icons'
+import debounce from 'lodash.debounce'
 
 import ProtectedRoute from 'components/ProtectedRoute'
 import BlogItem from './components/BlogItem'
 import { fetchBlogs, setCurrentPage } from 'redux/features/blogsSlice'
 import './styles.css'
 
-const LIMIT = 10
+const LIMIT = 2
 const getBlogs = (state) => state.blogs
 
 const Blogs = () => {
   const dispatch = useDispatch()
   const { list, loading, currentPage, total } = useSelector(getBlogs)
 
+  const [q, setQ] = useState('')
+  const [searchParams, setSearchParams] = useSearchParams()
+
   useEffect(() => {
-    const params = { page: currentPage, limit: LIMIT }
+    const pageFromURL = searchParams.has('page')
+      ? Number(searchParams.get('page'))
+      : 1
+    dispatch(setCurrentPage(pageFromURL))
+    const params = { page: currentPage, limit: LIMIT, q }
     dispatch(fetchBlogs(params))
-  }, [dispatch, currentPage])
+  }, [dispatch, currentPage, q, searchParams])
 
   const handlePageChange = (page) => {
     dispatch(setCurrentPage(page))
+    setSearchParams({ page })
   }
+
+  const handleSearch = debounce((event) => {
+    setQ(event.target.value)
+  }, 500)
 
   return (
     <ProtectedRoute>
@@ -32,6 +45,7 @@ const Blogs = () => {
           <Input
             style={{ width: '300px' }}
             placeholder="Search blog"
+            onChange={handleSearch}
             prefix={<SearchOutlined />}
           />
           <Link to="/blogs/create">
@@ -49,6 +63,7 @@ const Blogs = () => {
             pagination={{
               onChange: handlePageChange,
               pageSize: LIMIT,
+              current: currentPage,
               total,
             }}
             renderItem={(item) => <BlogItem item={item} />}
